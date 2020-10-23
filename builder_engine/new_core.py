9713e4bed81d70ee710b9637f932aca6b73e2b75
DiablosWhisper <shelyakingleb17@gmail.com>
2020-10-23 09:17:16 +0300

    update: BFS traverse

--- builder_engine/new_core.py
+++ builder_engine/new_core.py
@@ -1,11 +1,26 @@
 from typing import Dict, List, TypeVar
 
 Instance=TypeVar("Instance")
+Graph=TypeVar("Graph")
 Node=TypeVar("Node")
 
 class Graph(object):
-    #TODO Using bredth first search to set levels of each node(using setattr method)
-    #TODO Build layers using level of the current node
+    def _traverse(self, graph: Graph, root: Node):
+        """
+        Traverses graph using breadth first search
+        :param graph: collection of nodes
+        :param root: root of graph
+        """
+        """|Initializing container for algorithm|"""
+        visited, queue=[], []
+        visited.append(root)
+        queue.append(root)
+
+        """|Starting traversing|"""
+        while queue: 
+            [(visited.append(child), queue.append(child))
+            for child in graph[queue.pop(0)]
+            if child not in visited]
     class Node(object):
         def __new__(cls, config: Dict, instances: Dict,
         children: List[Node]=None)->Instance:
@@ -32,6 +47,14 @@ class Graph(object):
             cls._cast=config["cast"]
 
             return cls
+        def _del_child(self, child: Node):
+            """
+            Deletes child from children
+            :param child: node to delete
+            :return None
+            """
+            assert isinstance(child, self)
+            self.children.remove(child)
         def _add_child(self, child: Node):
             """
             Adds child to children
@@ -48,6 +71,13 @@ class Graph(object):
             return self._Builder(cast=self._cast,
             parameters=self._parameters,
             instances=self._instances)
+        def __del__(self):
+            """
+            Deletes node and its connections
+            :return None
+            """
+            [child._del_child(self)
+            for child in self.children]
         class _Builder(object):
             def __new__(cls, cast: str, parameters: Dict,
             instances: Dict)->Instance:
