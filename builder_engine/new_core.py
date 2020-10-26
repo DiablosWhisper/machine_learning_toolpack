445f985d277a729fb19030ea43cf6289d758cd82
DiablosWhisper <shelyakingleb17@gmail.com>
2020-10-26 21:19:00 +0200

    update: class Node

--- builder_engine/new_core.py
+++ builder_engine/new_core.py
@@ -1,66 +1,63 @@
-from typing import Dict, List, TypeVar
+from typing import Dict, TypeVar, List
 
 Instance=TypeVar("Instance")
+Node=TypeVar("Node")
 
 class Graph(object):
-    def __new__(cls)->Instance:
+    pass
+
+class Layer(object):
+    pass
+
+class Node(object):
+    def __init__(self, config: Dict, instances: Dict,
+    level: int, children: List[Node]=None)->None:
         """
+        Stores layer configuration in node
+        :param config: configuration of layer
+        :param nodes: nodes of node
+        :return None
         """
-        return
-    class _Branch(object):
-        def __new__(cls, configs: List[Dict],
-        level: int)->Instance:
-            """
-            """
-            return cls
-        class _Node(object):
-            def __new__(cls, config: Dict, 
-            instances: Dict)->Instance:
-                """
-                Stores layer configuration in node
-                :param config: configuration of layer
-                :param nodes: nodes of node
-                :return None
-                """
-                """|Saves parameters of layer|"""
-                cls._parameters={key: config[key]
-                for key in config
-                if key!="cast"}
+        self.children=([self._add_child(child)
+        for child in children]
+        if children else None)
 
-                """|Saves instances of layers|"""
-                cls._instances=instances
+        self._parameters={key: config[key]
+        for key in config
+        if key!="type"}
 
-                """|Saves type of layer|"""
-                cls._cast=config["cast"]
-
-                """|Builds layer(s)|"""
-                return cls._Builder(cast=cls._cast,
-                parameters=cls._parameters,
-                instances=cls._instances)
-            class _Builder(object):
-                def __new__(cls, cast: str, parameters: Dict,
-                instances: Dict)->Instance:
-                    """
-                    Build layer and returns its instance
-                    :param parameters: parameters of layer
-                    :param instances: instances of layers
-                    :param cast: cast of layer
-                    :return built layer
-                    """
-                    return instances[cast](**parameters)
-
-branch_1=[
-    dict(type="Input"),
-    dict(type="TimeDistributed", layer=dict(type="Dense")),
-    dict(type="Dense")
-]
-branch_2=[
-    dict(type="Input"),
-    dict(type="Bidirectional", layer=dict(type="Dense")),
-    dict(type="Dense")
-]
-root=[
-    dict(type="Concatenate", layer=[branch_1, branch_2]),
-    dict(type="Dense"),
-    dict(type="Dense")
-]
+        self._instances=instances
+        self._type=config["type"]
+        self._level=level
+    def _del_child(self, child: Node)->None:
+        """
+        Deletes child from children
+        :param child: node to delete
+        :return None
+        """
+        assert isinstance(child, self)
+        self.children.remove(child)
+    def _add_child(self, child: Node)->None:
+        """
+        Adds child to children
+        :param child: node to add
+        :return None
+        """
+        assert isinstance(child, self)
+        self.children.append(child)
+    def build(self)->Instance:
+        """
+        Builds layer in node
+        :return built layer
+        """
+        return Layer(type=self._type,
+        parameters=self._parameters,
+        instances=self._instances)
+    def __del__(self)->None:
+        """
+        Deletes node from graph
+        :return None
+        """
+        ([child._del_child(self)
+        for child in self.children] 
+        if self.children else None)
